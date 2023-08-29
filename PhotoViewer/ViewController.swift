4a27ee2da6560214854322b3248a445dd4ca9bbd
Nikita Makarevich <nikita-makarevich@mail.ru>
2023-08-30 01:15:23 +0300

    Initial Commit

---
+++ PhotoViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoViewer
+//
+//  Created by Никита Макаревич on 30.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
